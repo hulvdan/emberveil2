191c6d7a317378b0dd95b9bcd7f9afa1e553154d
hulvdan <Hulvdan@yandex.ru>
2025-07-13 18:52:54 +0300

    Removed game code

--- src/game/bf_game.cpp
+++ src/game/bf_game.cpp
@@ -9,42 +9,5 @@ const char* GetWindowTitle() {
 }
 
 SDL_AppResult GameUpdate() {
-  auto backgroundTexId = glib->background_texture_id();
-  auto backgroundTex   = glib->atlas_textures()->Get(backgroundTexId);
-
-  Vector2 size{(f32)backgroundTex->size_x(), (f32)backgroundTex->size_y()};
-  auto    scale = ScaleToFit(size, LOGICAL_RESOLUTION);
-
-  DrawTexture({
-    .texId = backgroundTexId,
-    .pos   = LOGICAL_RESOLUTION / 2,
-  });
-
-  DrawTexture({
-    .texId = glib->player_texture_id(),
-    .pos   = Vector2((f32)LOGICAL_RESOLUTION.x / 2, 0),
-    .anchor{0.5f, 0},
-  });
-  DrawTexture({
-    .texId = glib->player_texture_id(),
-    .pos   = Vector2(0, (f32)LOGICAL_RESOLUTION.y / 2),
-  });
-  DrawTexture({
-    .texId = glib->player_texture_id(),
-    .pos   = Vector2((f32)LOGICAL_RESOLUTION.x / 2, (f32)LOGICAL_RESOLUTION.y),
-  });
-  DrawTexture({
-    .texId = glib->player_texture_id(),
-    .pos   = Vector2((f32)LOGICAL_RESOLUTION.x, (f32)LOGICAL_RESOLUTION.y / 2),
-  });
-
-  EngineApplyVignette();
-
-  // .rotation   =0,
-  // .pos        ={},
-  // .anchor     =Vector2Half(),
-  // .scale      =,
-  // .sourceSize =,
-  // .color      =,
   return SDL_APP_CONTINUE;
 }
