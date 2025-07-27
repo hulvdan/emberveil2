9dcd19c94d6c0fac2a82b0b9fd801ffc44874744
hulvdan <Hulvdan@yandex.ru>
2025-07-27 14:35:57 +0300

    fix

--- vendor/gameanalytics-sdk-cpp/gtest_build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/user/dev/home/game-template/vendor/gameanalytics-sdk-cpp/externals/googletest
-# Build directory: C:/Users/user/dev/home/game-template/vendor/gameanalytics-sdk-cpp/gtest_build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- vendor/gameanalytics-sdk-cpp/gtest_build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/user/dev/home/game-template/vendor/gameanalytics-sdk-cpp/externals/googletest/googletest
-# Build directory: C:/Users/user/dev/home/game-template/vendor/gameanalytics-sdk-cpp/gtest_build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
