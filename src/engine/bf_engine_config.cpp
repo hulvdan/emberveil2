ed8f14e76c56e186e2bdc2ae360f573af49d13a1
hulvdan <Hulvdan@yandex.ru>
2026-01-06 10:00:54 +0500

    Update bf_engine_config.cpp

--- src/engine/bf_engine_config.cpp
+++ src/engine/bf_engine_config.cpp
@@ -3,9 +3,12 @@
 #define UI_PADDING_OUTER_VERTICAL (10)
 #define UI_PADDING_OUTER_HORIZONTAL (12)
 
-// e.g.
+// Example:
 // #define BF_ENGINE_EXTEND_CLAY_CUSTOM_DATA \
 //   X(ClayScreenBackground, screenBackground)
+//
+// ClayScreenBackground would need to have `bool set = false;`
+//
 #define BF_ENGINE_EXTEND_CLAY_CUSTOM_DATA
 
 ///
